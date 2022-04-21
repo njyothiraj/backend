7f875b1e7d9a3532545cbb69c99afa5cbac5ee89
vivjamba <85517762+vivjamba@users.noreply.github.com>
2022-04-21 16:08:28 -0400

    employee routes
    first draft for employee routes (contains all crud operations - to remove some later)

--- app.js
+++ app.js
@@ -1,26 +1,26 @@
 // Require Libraries
 const express = require('express');
 const knex = require('knex');
+
 // App Setup
 const knexConfig = require('./knexfile.js');
 const port = process.env.PORT || 3000;
-
-const app = express();
-const pool = knex(knexConfig);
+var app = express();
 
 // Middleware
 app.use(express.json()); // parse query string parameters as JSON (GET/DELETE)
 app.use(express.urlencoded({ extended: true })); // parse body parameters as JSON (POST/PUT)
 
-app.get('/', (req, res) => {
-  pool.select('*').from('employees').then((data) => {
-    res.status(200).json(data);
-  }).catch((err) => {
-    // Send a 500 (internal server error) if something goes wrong (rejected promises are not caught by express).
-    res.status(500).end();
-  });
-});
+
 // Routes
+//Save path of route files
+var employees = require('./routes/employees');
+
+//Url routes to file
+app.use('/employees',  employees);
+
+// const pool = knex(knexConfig);
+
 app.get('/', (req, res) => {
     res.send('Hello Squirrel');
   });
