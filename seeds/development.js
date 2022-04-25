f5c8d443aa6b817992e5b9097ab6fcfcb9a348af
vivjamba <85517762+vivjamba@users.noreply.github.com>
2022-04-25 14:48:49 -0400

    fixing env and pto schema

--- seeds/development.js
+++ seeds/development.js
@@ -24,7 +24,7 @@
   // ]);
   // await knex('pto').insert([
   //   //INSERT into pto values(1, 1, 3, 'vacation', 'going to Cancun', '2000-12-01', '2000-12-14', 1);
-  //   { id: 1, emp_id: 1, manager_id: 3, 'type': 'vacation', 'additional_info': 'going to Cancun', start_date: '2000-12-01', end_date: '2000-12-14', 1},
+  //   { id: 1, emp_id: 1, manager_id: 3, 'type': 'vacation', 'additional_info': 'going to Cancun', start_date: '2000-12-01', end_date: '2000-12-14', },
   //   { id: 2, emp_id: 2, manager_id: 4, 'type': 'medical', 'additional_info': 'doctors appt', start_date: '2000-01-04', end_date: '2000-01-06', 2}
   // ]);
 };
