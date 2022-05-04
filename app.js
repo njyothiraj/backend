610fcfb29722c4b7d428bc675120233addf59323
njyothiraj <99679887+njyothiraj@users.noreply.github.com>
2022-05-04 15:05:40 -0400

    neelimas changes

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const knex = require('knex');
 const knexConfig = require('./knexfile.js');
 const port = process.env.PORT || 3000;
 var app = express();
+var tester = {"slip":{"id":23,"advice":"Always bet on black in blackjack."}};
 
 // Middleware
 app.use(express.json()); // parse query string parameters as JSON (GET/DELETE)
 app.use(express.urlencoded({ extended: true })); // parse body parameters as JSON (POST/PUT)
 
 
+// import employees from 'employees'
 // Routes
 //Save path of route files
 
@@ -23,16 +25,69 @@ var assignedtraining = require('./routes/assignedtraining');
 var performance = require('./routes/performance');
 var login = require('./routes/login');
 
+
+
+// const pool = knex(knexConfig);
+
+// var express = require('express')
+const cors = require('cors')
+// var app = express()
+
+app.use(cors())
+
+app.use(function (req, res, next) {
+  const allowedOrigins = [
+    "http://localhost:3080/main#",
+    // "http://localhost:3000",
+  ];
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3080/main#');
+  }
+  // res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use('/pto',pto);
 app.use('/assignedtraining',assignedtraining);
 app.use('/employees',  employees);
 app.use('/performance', performance);
 app.use('/login', login);
 
-// const pool = knex(knexConfig);
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+// //   Access-Control-Allow-Origin: *
+// // Access-Control-Allow-Credentials: true
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+// app.listen(30000, function () {
+//   console.log('CORS-enabled web server listening on port 30000')
+// })
+
+// const cors = require('cors');
+// app.use(cors({
+//   origin: '*'
+// }));
 
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
+  res.send(tester);
+  // res.send('Hello Squirrel');
+  // res.send('{"slip":{"id":173,"advice":"Always bet on black."}}');
+});
+
+app.get('/employees', (req, res) => {
+    res.send('Test');
   });
 
 // Start Server
